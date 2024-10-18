bdea0f0b96afe9ded3933938da5f0fa03b2b8889
sergiocoba <146813201+sergiocoba@users.noreply.github.com>
2024-10-18 15:49:13 +0200

    MovimentJugador

--- Dodge/Player.gd
+++ Dodge/Player.gd
@@ -12,7 +12,24 @@ var screen_size # Mida de la finestra de joc.
 func _ready():
 	screen_size = get_viewport_rect().size
 
-
+func _process(delta):
+	var velocity = Vector2.ZERO # Vector de moviment del jugador.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
