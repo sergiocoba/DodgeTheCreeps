bcd988a265ee6202aee61f1b25c1936f95823891
sergiocoba <146813201+sergiocoba@users.noreply.github.com>
2024-10-18 15:42:05 +0200

    CreacióscriptJugador

---
+++ Dodge/Player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
+var screen_size # Mida de la finestra de joc.
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
